f23d0d37e6cc34bdeff8cd9e0fb46676332f5e60
Andrew <acastillonevarez@bsd220.org>
2023-03-01 09:29:37 -0600

    Initial Commit

---
+++ API Hackwich/API_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_HackwichApp.swift
+//  API Hackwich
+//
+//  Created by Andrew Castillo Nevarez on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct API_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
